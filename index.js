fe59674ecce5bd923d0bf46c4b252116192228c9
陈卓恺 <bruceczk@foxmail.com>
2018-10-02 13:16:27 +0800

    1.25.1

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const glob = require('glob')
+const execSync = require('child_process').execSync
+const fs = require('fs-extra')
+const defaultDir = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Europa Universalis IV'
+
+const version = process.argv[2]
+const gameDir = process.argv[3] || defaultDir
+
+if (!/^[.\d]+$/.test(version)) {
+  console.error('Please specify a valid version, eg. 1.26.1')
+  process.exit(1)
+}
+
+const filelist = fs.readFileSync(path.join(__dirname, 'filelist.txt')).toString().split(/\r?\n/).filter(line => line && line.trim())
+
+const files = []
+
+for (const item of filelist) {
+  const results = glob.sync(item, {
+    cwd: gameDir
+  })
+  files.push(...results)
+}
+
+for (const filename of files) {
+  fs.copySync(path.join(gameDir, filename), path.join(__dirname, filename))
+  console.log(filename, 'copied')
+}
+
+execSync(`git add .`)
+execSync(`git commit -am ${version}`)
